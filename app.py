8500ccd65b28c2b52549d9002497b1d942be6b28
Gabriel Gonçalves <gabrielp.goncalves1@gmail.com>
2026-02-11 15:10:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Base Ativar/Inativar Itens")
 
-st.selectbox("Opções",["TODAS LOJAS (ATACADO/VAREJO)","SOMENTE ATACADO","SOMENTE VAREJO","LOJAS ESPECÍFICAS"]
+st.selectbox("Opções",["TODAS LOJAS (ATACADO/VAREJO)","SOMENTE ATACADO","SOMENTE VAREJO","LOJAS ESPECÍFICAS"])
