3f184fc2869067c2b7b7bbbc518b73f3c06c365f
Gabriel Gonçalves <gabrielp.goncalves1@gmail.com>
2026-02-11 15:24:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,13 @@ import streamlit as st
 
 st.title("Base Ativar/Inativar Itens")
 st.text_input("SEQPRODUTO")
-st.selectbox("Opções",["TODAS LOJAS (ATACADO/VAREJO)","SOMENTE ATACADO","SOMENTE VAREJO","LOJAS ESPECÍFICAS"])
+opcao = st.selectbox("Opções",
+             ["TODAS LOJAS (ATACADO/VAREJO)",
+              "SOMENTE ATACADO","SOMENTE VAREJO",
+              "LOJAS ESPECÍFICAS"])
+if opcao == "LOJAS ESPECÍFICAS":
+  lojas = st.selectbox("Escolha a Filial",
+                       ["1","3","5","7","9","11","12","13","14","15","16","19","25","26",
+                        "27","28","29","30","31","32","33","34","35","41","43","44","45",
+                        "47","48","50","51","53","54","55","56","57","58","201","202","203",
+                        "204"]
