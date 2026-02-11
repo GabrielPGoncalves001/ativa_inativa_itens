8ea3c541e8bcfd6bfea283d37e89115cfa28c144
Gabriel Gonçalves <gabrielp.goncalves1@gmail.com>
2026-02-11 16:11:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,30 +5,22 @@ from datetime import datetime
 agora = datetime.now().strftime('%d/%m/%Y %H:%M')
 st.title("Base Ativar/Inativar Itens")
 
-  
 seqproduto = st.text_input("SEQPRODUTO")
-
 seleciona_lojas = st.selectbox("Opções",
              ["TODAS LOJAS (ATACADO/VAREJO)",
               "SOMENTE ATACADO","SOMENTE VAREJO",
               "LOJAS ESPECÍFICAS"])
-
-
-
 lojas = None
-
 if seleciona_lojas == "LOJAS ESPECÍFICAS":
   lojas = st.selectbox("Escolha a Filial",
                        ["1","3","5","7","9","11","12","13","14","15","16","19","25","26",
                         "27","28","29","30","31","32","33","34","35","41","43","44","45",
                         "47","48","50","51","53","54","55","56","57","58","201","202","203",
                         "204"])
-
+  
 opcao = st.selectbox("Selecione a opção", ["ATIVAR","INATIVAR"])
 
-
 if st.button("SALVAR"):
-  
   st.write(f'Base Salva em: {agora}')  
   seq_list = [s.strip() for s in seqproduto.split(",") if s.strip() != ""]
   
@@ -40,7 +32,7 @@ if st.button("SALVAR"):
     "STATUSCOMPRA":[opcao]*len(seq_list),
     "STATUSVENDA":"A"
   }
-
+  
   csv = pd.DataFrame(dados)
 
   csv_bytes = csv.to_csv(index=False, sep=';').encode("utf=8")
