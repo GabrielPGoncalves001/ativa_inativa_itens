60e3661acd3246e4bb8711ff9bd84a3956186080
Gabriel Gonçalves <gabrielp.goncalves1@gmail.com>
2026-02-11 15:54:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,30 @@ import pandas as pd
 
 st.title("Base Ativar/Inativar Itens")
 
-with st.form("Base"):
-  
-  seqproduto = st.text_input("SEQPRODUTO")
-  
-  seleciona_lojas = st.selectbox("Opções",
-               ["TODAS LOJAS (ATACADO/VAREJO)",
-                "SOMENTE ATACADO","SOMENTE VAREJO",
-                "LOJAS ESPECÍFICAS"])
-  
-  
-  
-  lojas = None
-  
-  if seleciona_lojas == "LOJAS ESPECÍFICAS":
-    lojas = st.selectbox("Escolha a Filial",
-                         ["1","3","5","7","9","11","12","13","14","15","16","19","25","26",
-                          "27","28","29","30","31","32","33","34","35","41","43","44","45",
-                          "47","48","50","51","53","54","55","56","57","58","201","202","203",
-                          "204"])
 
-  opcao = st.selectbox("Selecione a opção", ["ATIVAR","INATIVAR"])
-  enviar = st.form_submit_button("Criar Base CSV")
+  
+seqproduto = st.text_input("SEQPRODUTO")
 
-if enviar:
+seleciona_lojas = st.selectbox("Opções",
+             ["TODAS LOJAS (ATACADO/VAREJO)",
+              "SOMENTE ATACADO","SOMENTE VAREJO",
+              "LOJAS ESPECÍFICAS"])
+
+
+
+lojas = None
+
+if seleciona_lojas == "LOJAS ESPECÍFICAS":
+  lojas = st.selectbox("Escolha a Filial",
+                       ["1","3","5","7","9","11","12","13","14","15","16","19","25","26",
+                        "27","28","29","30","31","32","33","34","35","41","43","44","45",
+                        "47","48","50","51","53","54","55","56","57","58","201","202","203",
+                        "204"])
+
+opcao = st.selectbox("Selecione a opção", ["ATIVAR","INATIVAR"])
+
+
+if st.button("Gerar Base CSV"):
   seq_list = [s.strip() for s in seqproduto.split(",") if s.strip() != ""]
   
   lojas_final = lojas if lojas else seleciona_lojas
