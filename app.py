516a5291de2281b32dace6e0be7e61f51c59178e
Gabriel Gonçalves <gabrielp.goncalves1@gmail.com>
2026-02-11 15:14:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Base Ativar/Inativar Itens")
-
+st.text_input("SEQPRODUTO")
 st.selectbox("Opções",["TODAS LOJAS (ATACADO/VAREJO)","SOMENTE ATACADO","SOMENTE VAREJO","LOJAS ESPECÍFICAS"])
