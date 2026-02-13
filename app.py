9073fc0c3df5f7af77d616a4577ff378045be9c6
Gabriel Gonçalves <gabrielp.goncalves1@gmail.com>
2026-02-13 15:21:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ seleciona_lojas = st.selectbox("Opções",
               "LOJAS ESPECÍFICAS"])
 lojas = None
 if seleciona_lojas == "LOJAS ESPECÍFICAS":
-  lojas = st.selectbox("Escolha a Filial",
+  lojas = st.multiselect("Escolha a(s) Filial(is)",
                        ["1","3","5","7","9","11","12","13","14","15","16","19","25","26",
                         "27","28","29","30","31","32","33","34","35","41","43","44","45",
                         "47","48","50","51","53","54","55","56","57","58","201","202","203",
